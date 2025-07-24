53705ba7bf0f45094daef62bd81363617aa5d8d5
Valeri Goltsman <valerig1901@gmail.com>
2025-07-25 02:54:57 +0300

    Change default

--- variables.tf
+++ variables.tf
@@ -67,7 +67,7 @@ variable "github_org" {
 variable "github_repo" {
   description = "GitHub repository name"
   type        = string
-  default = "openID-test"
+  default = "GitHubA2AWS"
 }
 
 variable "github_branch_pattern" {
